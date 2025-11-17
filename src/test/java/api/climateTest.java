9c0ae5316044fb53beb19658a69bc4bf79f1ee55
matt_ush <matiasrtripodi@gmail.com>
2025-11-17 10:09:02 -0300

    modificacion test

--- src/test/java/api/climateTest.java
+++ src/test/java/api/climateTest.java
@@ -1,15 +1,25 @@
-@Test
-public void testWeatherCordoba() {
-    String apiKey = System.getenv("WEATHER_API_KEY");
+package api;
 
-    RestAssured
-        .given()
-        .queryParam("q", "Cordoba")
-        .queryParam("appid", apiKey)
-        .when()
-        .get("https://api.openweathermap.org/data/2.5/weather")
-        .then()
-        .statusCode(200)
-        .body("name", equalTo("Cordoba"))
-        .body("main.temp", notNullValue());
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ClimateTest {
+
+    @Test
+    public void testWeatherCordoba() {
+        String apiKey = System.getenv("WEATHER_API_KEY");
+
+        RestAssured
+            .given()
+            .queryParam("q", "Cordoba")
+            .queryParam("appid", apiKey)
+            .when()
+            .get("https://api.openweathermap.org/data/2.5/weather")
+            .then()
+            .statusCode(200)
+            .body("name", equalTo("Cordoba"))
+            .body("main.temp", notNullValue());
+    }
 }
